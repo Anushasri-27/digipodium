4f9092ff4e1fe94b1de58aad84ad4ff72592993f
Anushasri-27 <anushasrivastava1999@gmail.com>
2023-09-26 12:14:38 +0530

    basci.j

---
+++ basic_js/new.js
@@ -0,0 +1,9 @@
+let price= [1000 ,3000,500,400,200,5000,1200];
+
+let New = price.filter((x)=> x>=1000 && x<=3000);
+
+console.log(New);
+
+let color = ["red" ,"orange" ,"yellow","blue"];
+let newColor = color.filter((x) => x.length >4);
+console.log(newColor);
